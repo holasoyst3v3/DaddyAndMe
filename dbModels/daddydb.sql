b9104b58ef34cd0e0e93c8fd6501270653c45f57
holasoyst3v3 <s.ljr.estrada@gmail.com>
2021-12-17 03:00:21 -0600

    moved Guest In from all headers to auth.html, added 'posts'

--- dbModels/daddydb.sql
+++ dbModels/daddydb.sql
@@ -2,27 +2,29 @@ CREATE TABLE auth ( auth_id SERIAL PRIMARY KEY, hash STRING, dad_id INTEGER FORE
 CREATE TABLE dad ( person_id SERIAL PRIMARY KEY, firstName VARCHAR(200), lastName VARCHAR(200), email VARCHAR(200), username VARCHAR(200));
 CREATE TABLE post ( person_id SERIAL PRIMARY KEY, name VARCHAR(200), age INTEGER, height INTEGER, city VARCHAR(200), favorite_color VARCHAR(200));
 
-INSERT INTO auth ( hash, dad_id, email) VALUES ({hash}, {dad_id}, {email});
+-- INSERT INTO auth ( hash, dad_id, email) VALUES ({hash}, {dad_id}, {email});
 
 
 CREATE TABLE dad (
     dad_id SERIAL PRIMARY KEY,
-    title VARCHAR(50)
+    firstName VARCHAR(200),
+    lastName VARCHAR(200),
+    email VARCHAR(200),
+    username VARCHAR(200)
 );
 
 CREATE TABLE auth (
     auth_id SERIAL PRIMARY KEY,
     dad_id INTEGER NOT NULL REFERENCES dad(dad_id),
     hash 
-    email VARCHAR(50)
+    email VARCHAR(50) REFERENCES dad(email)
 );
 
 CREATE TABLE post (
     post_id SERIAL PRIMARY KEY,
     postcontent TEXT,
     username INTEGER NOT NULL REFERENCES users(user_id),
-    body TEXT
 );
 
 
-module.exports = auth;
+module.exports = daddydb;
--- dbModels/dads.sql
+++ dbModels/dads.sql
@@ -1,2 +1,1 @@
-CREATE TABLE person ( person_id SERIAL PRIMARY KEY, firstName VARCHAR(200), lastName VARCHAR(200), email VARCHAR(200), username VARCHAR(200));
-
+-- NOT IN USE --
